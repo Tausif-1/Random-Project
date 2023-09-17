04fa3a3d5f4cac5ffc70633f998fc77292ba06e0
tausif <iamtausif47@gmail.com>
2023-09-17 13:51:37 +0530

    create a simple webpage structure

---
+++ simple webpage/nav.js
@@ -0,0 +1,12 @@
+const pic = document.querySelector("#img1");
+
+pic.addEventListener("click",() => {
+    pic.style.transform = "rotate(20deg)"
+})
+
+const btn = document.querySelector("#log");
+
+btn.addEventListener("click",() => {
+    pic.style.transform = "rotate(20deg)"
+})
+
